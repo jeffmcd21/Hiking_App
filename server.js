b5051452aee4ddbb73375d54615a0c943ffcfcdb
Jeff McDonald <jwmcdonald85@gmail.com>
2025-11-19 12:49:36 -0500

    refactor middleware

--- server.js
+++ server.js
@@ -4,13 +4,7 @@
 // ---------- ---------- ---------- ---------- ---------- //
 require('dotenv').config(); 
 const express = require('express');
-const morgan = require('morgan');
-const methodOverride = require('method-override');
-const hikeController = require('./controllers/hike');
-const userController = require('./controllers/user');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-
+const registerGlobalMiddleware = require('./utils/middleware')
 
 // ---------- ---------- ---------- ---------- ---------- //
 // ~ EXPRESS APP CONFIGURATION ~ //
@@ -19,20 +13,9 @@ const app = express();
 
 
 // ---------- ---------- ---------- ---------- ---------- //
-// ~ MIDDLEWARE ~ //
+// ~ REGISTER GLOBAL MIDDLEWARE ~ //
 // ---------- ---------- ---------- ---------- ---------- //
-app.use(morgan('dev'));
-app.use(methodOverride('_method'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use(session({
-    secret: process.env.SECRET,
-    store: MongoStore.create({ mongoUrl: process.env.DATABASE_URL }),
-    saveUninitialized: true,
-    resave: false,
-}));
-app.use('/hikes', hikeController);
-app.use('/user', userController);
+registerGlobalMiddleware(app);
 
 
 // ---------- ---------- ---------- ---------- ---------- //
---
+++ utils/middleware.js
@@ -0,0 +1,37 @@
+
+// ---------- ---------- ---------- ---------- ---------- //
+// ~ IMPORT DEPENDENCIES ~ //
+// ---------- ---------- ---------- ---------- ---------- //
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const methodOverride = require('method-override');
+const hikeController = require('../controllers/hike');
+const userController = require('../controllers/user');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
+
+// ---------- ---------- ---------- ---------- ---------- //
+// ~ MIDDLEWARE FUNCTION ~ //
+// ---------- ---------- ---------- ---------- ---------- //
+function registerGlobalMiddleware(app) {
+    app.use(morgan('dev'));
+    app.use(methodOverride('_method'));
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.static('public'));
+    app.use(session({
+        secret: process.env.SECRET,
+        store: MongoStore.create({ mongoUrl: process.env.DATABASE_URL }),
+        saveUninitialized: true,
+        resave: false,
+    }));
+    app.use('/hikes', hikeController);
+    app.use('/user', userController);
+}
+
+
+// ---------- ---------- ---------- ---------- ---------- //
+// ~ EXPORT MIDDLEWARE FUNCTION ~ //
+// ---------- ---------- ---------- ---------- ---------- //
+module.exports = registerGlobalMiddleware;
